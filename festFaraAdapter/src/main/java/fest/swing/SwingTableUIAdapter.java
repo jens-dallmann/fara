ca694a9025bd81a93b0f85ea25ed62f7f93c0a74
dacendaran <dacendaran@c8a77015-025f-45f0-9547-fc4222d1e9d2>
2013-06-17 18:06:35 +0000

    added command

--- festFaraAdapter/src/main/java/fest/swing/SwingTableUIAdapter.java
+++ festFaraAdapter/src/main/java/fest/swing/SwingTableUIAdapter.java
@@ -1,5 +1,8 @@
 package fest.swing;
 
+import static org.fest.swing.data.TableCellByColumnId.row;
+
+import org.fest.swing.fixture.JTableCellFixture;
 import org.fest.swing.fixture.JTableFixture;
 
 import fest.FestResultBuilder;
@@ -19,7 +22,10 @@ public class SwingTableUIAdapter implements HasCommands, TableUIAdapter {
 		_frameWrapper = frameWrapper;
 	}
 
-	@FitCommand({"The name of the table which rows should be checked", "The operator which is used for check. For valid Operators see Operatorlist", "The number used for comparison"})
+	@FitCommand({
+			"The name of the table which rows should be checked",
+			"The operator which is used for check. For valid Operators see Operatorlist",
+			"The number used for comparison" })
 	public CommandResult checkTableRowCount(String tableName, String operator,
 			String expected) {
 		CommandResult result = new CommandResult();
@@ -28,32 +34,58 @@ public class SwingTableUIAdapter implements HasCommands, TableUIAdapter {
 			int rowCountInput = ParseUtils.readIntegerInput(expected, result);
 			int rowCount = table.rowCount();
 			if (result.getResultState() != CommandResultState.WRONG) {
-				RelationalOperatorEvaluator.evaluateOperation(operator, rowCount, rowCountInput, result);
+				RelationalOperatorEvaluator.evaluateOperation(operator,
+						rowCount, rowCountInput, result);
 			}
 		}
 		return result;
 	}
-	@FitCommand({"Name of the table in which the cell should be checked",
-		"The row number",
-		"The column number",
-		"The expected String"})
-	public CommandResult checkTableCell(String tableName, String row, String column, String expected) {
+
+	@FitCommand({ "Name of the table in which the row should be selected",
+			"The row which should be selected","the column where the mouse should click on" })
+	public CommandResult selectTableRow(String tableName, String row, String columnName) {
+		CommandResult result = new CommandResult();
+		int rowNumber = Integer.parseInt(row);
+		JTableFixture table = findTable(tableName, result);
+		if (CommandResultState.WRONG != result.getResultState()) {
+			try {
+//				table.selectRows(rowNumber-1);
+				JTableCellFixture cell = table.cell(row(rowNumber-1).columnId(columnName));
+				cell.click();
+				result.setResultState(CommandResultState.RIGHT);
+			} catch (Exception e) {
+				result.setFailureMessage(e.getMessage());
+				result.setResultState(CommandResultState.WRONG);
+				result.setWrongParameterNumber(2);
+			}
+		}
+		return result;
+	}
+
+	@FitCommand({ "Name of the table in which the cell should be checked",
+			"The row number", "The column number", "The expected String" })
+	public CommandResult checkTableCell(String tableName, String row,
+			String column, String expected) {
 		CommandResult result = new CommandResult();
 		int rowNumber = Integer.parseInt(row);
 		int columnNumber = Integer.parseInt(column);
 		JTableFixture table = findTable(tableName, result);
-		String[][] contents = table.contents();
-		String actual = contents[rowNumber-1][columnNumber-1];
-		if(expected.equals(actual)) {
-			result.setResultState(CommandResultState.RIGHT);
-		}
-		else {
-			result.setFailureMessage("The text in row "+row+" and column "+column+" is \""+actual+"\" and does not match \""+expected+"\"");
-			result.setResultState(CommandResultState.WRONG);
-			result.setWrongParameterNumber(4);
+		if (result.getResultState() != CommandResultState.WRONG) {
+			String[][] contents = table.contents();
+			String actual = contents[rowNumber - 1][columnNumber - 1];
+			if (expected.equals(actual)) {
+				result.setResultState(CommandResultState.RIGHT);
+			} else {
+				result.setFailureMessage("The text in row " + row
+						+ " and column " + column + " is \"" + actual
+						+ "\" and does not match \"" + expected + "\"");
+				result.setResultState(CommandResultState.WRONG);
+				result.setWrongParameterNumber(4);
+			}
 		}
 		return result;
 	}
+
 	private JTableFixture findTable(String tableName, CommandResult result) {
 		try {
 			return _frameWrapper.getFrameFixture().table(tableName);
@@ -64,5 +96,4 @@ public class SwingTableUIAdapter implements HasCommands, TableUIAdapter {
 		}
 	}
 
-	
 }
