bc450551f724c61db42c60216596f7600f41d1e3
Jens Dallmann <8dallman@informatik.uni-hamburg.de>
2013-12-09 14:40:53 +0100

    fara test runner will consolidate the files from now on fara test runner will use a property file to read configurable folders

--- FaraTestapp/faraTestRunner-maven-plugin/src/main/java/FaraTestRunner.java
+++ FaraTestapp/faraTestRunner-maven-plugin/src/main/java/FaraTestRunner.java
@@ -1,8 +1,9 @@
 import core.ClassLoaderUtils;
 import core.ProcessListener;
 import core.service.FitIOService;
+import core.service.PropertyService;
 import core.service.exceptions.CreateDirectoryException;
-import directoryCrawler.CrawlerAction;
+import core.service.exceptions.FitIOException;
 import directoryCrawler.DirectoryCrawler;
 import directoryCrawler.NoActionsToExecuteException;
 import fit.Parse;
@@ -15,6 +16,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 
+import javax.xml.bind.PropertyException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,18 +24,15 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Deception
- * Date: 26.07.13
- * Time: 19:51
- * To change this template use File | Settings | File Templates.
- *
+ * @threadSafe false
  * @goal test
  * @phase integration-test
  * @requiresDependencyResolution test
  * @requiresDependencyCollection test
  */
 public class FaraTestRunner extends AbstractMojo {
+  private static final String CONSOLIDATE_RESULT_FOLDER = "consolidated_result_folder";
+  private static final String TEST_DIRECTORY_FOLDER = "test_directory_folder";
   /**
    * Directory where compiled classes will be
    *
@@ -61,13 +60,37 @@ public class FaraTestRunner extends AbstractMojo {
   private MavenProject project;
 
   /**
+   * The directory path were test files are stored
+   * can be passed as maven parameter or set in user home .fara-test-configuration.properties with test_directory_folder = "/any/absolute/path"
+   * <p/>
+   * the maven parameter has a higher priority than the configuration in property file
+   *
    * @parameter
    */
   private String faraTestsDirectoryPath;
 
+  /**
+   * The directory were all result files should be stored.
+   * can be passed as maven parameter or set in user home .fara-test-configuration.properties with  consolidated_result_folder = "/any/absolute/path"
+   * <p/>
+   * the maven parameter has a higher priority than the configuration in property file
+   * the default value is: /target/faraTestResults
+   *
+   * @parameter
+   */
+  private String consolidatedResultFolder;
+  private String propertyFile;
+
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-    List<File> allTestFiles = collectTestFiles();
+    String userHome = System.getProperties().getProperty("user.home");
+    propertyFile = userHome + File.separator + ".fara-test-configuration.properties";
+    List<File> allTestFiles = null;
+    try {
+      allTestFiles = collectTestFiles();
+    } catch (PropertyException e) {
+      throw new MojoExecutionException(e.getMessage());
+    }
     List<AbstractActionFixtureAggregator> testProcessors = scanForAbstractFixtureAggregator();
     getLog().info("Found " + testProcessors.size() + " possible test processors");
     List<Pair> pairs = buildPairs(testProcessors, allTestFiles);
@@ -77,7 +100,6 @@ public class FaraTestRunner extends AbstractMojo {
       for (File testFile : testFiles) {
         try {
           FitIOService service = new FitIOService();
-          System.out.println(testFile.getAbsolutePath());
           service.createResultDirectory(testFile);
           String testFileContent = FileUtils.readFileToString(testFile);
           Parse htmlTable = new Parse(testFileContent);
@@ -87,12 +109,16 @@ public class FaraTestRunner extends AbstractMojo {
             testProcessor.doRow(processableRow);
             processableRow = processableRow.more;
           }
+          getLog().info("Write result file for: " + testFile.getAbsolutePath());
+          service.writeTestResult(testFile, firstProcessableRow, htmlTable.at(0, 0, 0).text());
         } catch (IOException e) {
           getLog().warn("Can not load testfile " + testFile.getAbsolutePath() + " while try to process it");
         } catch (FitParseException e) {
           getLog().warn("Can not parse testfile " + testFile.getAbsolutePath() + " while try to process it");
         } catch (CreateDirectoryException e) {
-          getLog().warn("Can not create result directory for file: "+testFile.getAbsolutePath());
+          getLog().warn("Can not create result directory for file: " + testFile.getAbsolutePath());
+        } catch (FitIOException e) {
+          e.printStackTrace();
         }
       }
     }
@@ -105,16 +131,56 @@ public class FaraTestRunner extends AbstractMojo {
 
   private void consolidateResultFiles() throws NoActionsToExecuteException {
     DirectoryCrawler crawler = new DirectoryCrawler();
-    File testdirectoryFolder = new File(faraTestsDirectoryPath);
-    CopyResultFileCrawlerAction action = new CopyResultFileCrawlerAction(new File("/home/deception/result"));
-    crawler.crawlDirectory(testdirectoryFolder, null, action, true);
+    File testDirectoryFolder = new File(faraTestsDirectoryPath);
+    File resultDirectory = readResultDirectory();
+    CopyResultFileCrawlerAction action = new CopyResultFileCrawlerAction(resultDirectory);
+    crawler.crawlDirectory(testDirectoryFolder, null, action, true);
   }
+
+  private File readResultDirectory() {
+    if (consolidatedResultFolder == null) {
+      getLog().info("no folder for test results set in maven property. Try to use property file");
+      String targetDirectory = getTargetDirectory();
+      File defaultResultFolder = new File(targetDirectory + File.separator + "faraTestResults" + File.separator);
+      File file = new File(propertyFile);
+      if (file.exists()) {
+        try {
+          PropertyService service = new PropertyService(propertyFile);
+          String consolidateResultFolder = service.getProperty(CONSOLIDATE_RESULT_FOLDER);
+          if (consolidateResultFolder != null) {
+            getLog().info("Found result folder in property file. Will use: " + consolidateResultFolder);
+            return new File(consolidateResultFolder);
+          }
+        } catch (PropertyException e) {
+          getLog().info("Failed to read Property for result folder. Will use default result folder for consolidation: " + defaultResultFolder.getAbsolutePath());
+          return defaultResultFolder;
+        }
+        getLog().info("Failed to read Property for result folder. Will use default result folder for consolidation: " + defaultResultFolder.getAbsolutePath());
+        return defaultResultFolder;
+      } else {
+        getLog().info("No property file exist in user.home. Will use default result folder for consolidation: " + defaultResultFolder.getAbsolutePath());
+        return defaultResultFolder;
+      }
+    } else {
+      getLog().info("Use maven property for test results: " + consolidatedResultFolder);
+      return new File(consolidatedResultFolder);
+    }
+  }
+
+  private String getTargetDirectory() {
+    String[] directoryPath = outputDirectory.split(File.separator);
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < directoryPath.length - 1; i++) {
+      builder.append(directoryPath[i]).append(File.separator);
+    }
+    return builder.toString();
+  }
+
   private AbstractActionFixtureAggregator configureFixtureAggregator(Pair onePair) {
     AbstractActionFixtureAggregator testProcessor = onePair.getFixtureAggregator();
     testProcessor.registerProcessListener(new ProcessListener() {
       @Override
       public void publishResult(String state, String message) {
-        getLog().info(state);
       }
 
       @Override
@@ -176,7 +242,7 @@ public class FaraTestRunner extends AbstractMojo {
     return testProcessors;
   }
 
-  private List<File> collectTestFiles() throws MojoFailureException {
+  private List<File> collectTestFiles() throws MojoFailureException, PropertyException {
     List<File> testFiles = new ArrayList<File>();
     File sourceDirectory = createAndValidateSourceDirectory();
     getLog().info("Try to load test files from: " + sourceDirectory.getAbsolutePath());
@@ -185,16 +251,39 @@ public class FaraTestRunner extends AbstractMojo {
     return testFiles;
   }
 
-  private File createAndValidateSourceDirectory() throws MojoFailureException {
-    File sourceDirectory = new File(faraTestsDirectoryPath);
-    getLog().info("Test Directory is: " + sourceDirectory.getAbsolutePath());
-    if (!sourceDirectory.exists()) {
-      throw new MojoFailureException("Testdirectory \"" + sourceDirectory.getAbsolutePath() + " \"");
+  private File createAndValidateSourceDirectory() throws MojoFailureException, PropertyException {
+    File testDirectory;
+    if (faraTestsDirectoryPath != null) {
+      getLog().info("Maven Property found for test directory: " + faraTestsDirectoryPath);
+      testDirectory = new File(faraTestsDirectoryPath);
+    } else {
+      getLog().info("Try to read test directory from property file");
+      testDirectory = readTestDirectoryFromProperty();
+      if (testDirectory != null) {
+        getLog().info("Will use test directory from Property file: " + testDirectory.getAbsolutePath());
+      }
     }
-    if (!sourceDirectory.isDirectory()) {
-      throw new MojoFailureException("Given Testdirectory \"" + sourceDirectory.getAbsolutePath() + "\" is not a directory");
+    if (testDirectory == null) {
+      throw new MojoFailureException("No test directory as Maven Parameter and no test directory set in property file.");
     }
-    return sourceDirectory;
+    faraTestsDirectoryPath = testDirectory.getAbsolutePath();
+    getLog().info("Test Directory is: " + testDirectory.getAbsolutePath());
+    if (!testDirectory.exists()) {
+      throw new MojoFailureException("Testdirectory \"" + testDirectory.getAbsolutePath() + " \"");
+    }
+    if (!testDirectory.isDirectory()) {
+      throw new MojoFailureException("Given Testdirectory \"" + testDirectory.getAbsolutePath() + "\" is not a directory");
+    }
+    return testDirectory;
+  }
+
+  private File readTestDirectoryFromProperty() throws PropertyException {
+    PropertyService propertyService = new PropertyService(propertyFile);
+    String test_folder_path = propertyService.getProperty(TEST_DIRECTORY_FOLDER);
+    if (test_folder_path == null) {
+      return null;
+    }
+    return new File(test_folder_path);
   }
 
   private Collection<? extends File> collectTestFiles(File sourceDirectory) {
@@ -204,8 +293,12 @@ public class FaraTestRunner extends AbstractMojo {
         testFiles.addAll(collectTestFiles(fileInDirectory));
       }
     } else {
-      testFiles.add(sourceDirectory);
-      getLog().info("Loaded Testfile: " + sourceDirectory.getAbsolutePath());
+      boolean isHtmlFile = sourceDirectory.getAbsolutePath().endsWith(".html");
+      boolean isResultFile = sourceDirectory.getAbsolutePath().endsWith("_result.html");
+      if (isHtmlFile && !isResultFile) {
+        testFiles.add(sourceDirectory);
+        getLog().info("Loaded Testfile: " + sourceDirectory.getAbsolutePath());
+      }
     }
 
     return testFiles;
