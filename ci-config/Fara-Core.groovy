702866339e93312c0d3d01e0a4b97dd557b35475
dace <jens.dallmann@gmx.de>
2013-12-15 15:44:53 +0100

    added cobertura

--- ci-config/Fara-Core.groovy
+++ ci-config/Fara-Core.groovy
@@ -10,6 +10,20 @@ job {
         maven("clean install -f Fara-Core/pom.xml", "Fara-Core/pom.xml")
     }
     publishers {
+        cobertura("**/cobertura.xml") {
+            onlyStable(false)    // Include only stable builds, i.e. exclude unstable and failed ones.
+            failUnhealthy(false) // Unhealthy projects will be failed.
+            failUnstable(false)  // Unstable projects will be failed.
+            autoUpdateHealth(false)    // Auto update threshold for health on successful build.
+            autoUpdateStability(false) // Auto update threshold for stability on successful build.
+            zoomCoverageChart(false)   // Zoom the coverage chart and crop area below the minimum and above the maximum coverage of the past reports.
+            failNoReports(true) // Fail builds if no coverage reports have been found.
+            sourceEncoding('ASCII') // Character encoding of source files
+            // The following targets are added by default to check the method, line and conditional level coverage:
+            methodTarget(80, 0, 0)
+            lineTarget(80, 0, 0)
+            conditionalTarget(70, 0, 0)
+        }
         chucknorris()
     }
 }
