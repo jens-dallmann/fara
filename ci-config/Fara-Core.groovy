a0709952db1657452066217bd450dc1b23d56632
dace <jens.dallmann@gmx.de>
2013-12-17 19:41:09 +0100

    added cobertura job

--- ci-config/Fara-Core.groovy
+++ ci-config/Fara-Core.groovy
@@ -9,6 +9,19 @@ job {
     steps {
         maven("clean install -f Fara-Core/pom.xml", "Fara-Core/pom.xml")
     }
+}
+
+job {
+    name 'Fara-Core'
+    scm {
+        git('git://github.com/Dace/fara.git')
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        maven("cobertura:cobertura -Dcobertura", "Fara-Core/pom.xml")
+    }
     publishers {
         cobertura("**/cobertura.xml") {
             onlyStable(false)    // Include only stable builds, i.e. exclude unstable and failed ones.
@@ -20,9 +33,9 @@ job {
             failNoReports(true) // Fail builds if no coverage reports have been found.
             sourceEncoding('ASCII') // Character encoding of source files
             // The following targets are added by default to check the method, line and conditional level coverage:
-            methodTarget(80, 0, 0)
-            lineTarget(80, 0, 0)
-            conditionalTarget(70, 0, 0)
+            methodTarget(0, 0, 0)
+            lineTarget(0, 0, 0)
+            conditionalTarget(0, 0, 0)
         }
         chucknorris()
     }
