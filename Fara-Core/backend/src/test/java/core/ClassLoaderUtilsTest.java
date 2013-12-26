5613cd69a933d19a729afacce9cae521cbf0d613
dace <jens.dallmann@gmx.de>
2013-12-26 18:08:40 +0100

    static import of mockito method

--- Fara-Core/backend/src/test/java/core/ClassLoaderUtilsTest.java
+++ Fara-Core/backend/src/test/java/core/ClassLoaderUtilsTest.java
@@ -2,6 +2,7 @@ package core;
 
 import directoryCrawler.CrawlerAction;
 import directoryCrawler.DirectoryCrawler;
+import directoryCrawler.NoActionsToExecuteException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -13,6 +14,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.isNull;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -30,7 +32,7 @@ public class ClassLoaderUtilsTest {
   public void testLoadClassesRecursivelyFromDirectory() throws Exception {
     File file = mock(File.class);
     List<Class<?>> classes = ClassLoaderUtils.loadClassesRecursivelyFromDirectory(crawler, mock(ClassLoader.class), file);
-    verify(crawler, times(1)).crawlDirectory(eq(file), Mockito.isNull(CrawlerAction.class), any(LoadClassCrawlerAction.class), eq(true));
+    verify(crawler, times(1)).crawlDirectory(eq(file), isNull(CrawlerAction.class), any(LoadClassCrawlerAction.class), eq(true));
     assertTrue(classes != null);
     assertEquals(0, classes.size());
   }
