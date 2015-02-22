1d5dfb199cd907f8a854ce286e1b14e2a676ffd2
Hudson <hudson@mydomain.com>
2015-02-22 16:17:18 +0100

    clean up verify tasks

--- Tools/fitCommandDocumentor/src/it/create-one-documentation-file-custom-target/verify.groovy
+++ Tools/fitCommandDocumentor/src/it/create-one-documentation-file-custom-target/verify.groovy
@@ -4,10 +4,6 @@ import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 import org.xml.sax.InputSource
 
-import javax.xml.xpath.XPath
-import javax.xml.xpath.XPathConstants
-import javax.xml.xpath.XPathExpression
-import javax.xml.xpath.XPathFactory
 import java.io.*;
 
 File file = new File(basedir, "target/custom/FitCommands/firstIntegrationTestFitCommandDocs.html")
@@ -15,21 +11,13 @@ if (!file.exists()) {
   throw new FileNotFoundException("Can not find generated documentation file in target/custom/FitCommands/firstIntegrationTestFitCommandDocs.html");
 }
 
-XPathFactory xpFactory = XPathFactory.newInstance();
-XPath xpath = xpFactory.newXPath();
-String expression = "/html/body/table";
-XPathExpression xpathExpression = xpath.compile(expression);
-
 DOMParser parser = new DOMParser();
 parser.setFeature("http://xml.org/sax/features/namespaces", false);
 parser.parse(new InputSource(new FileInputStream(file)));
 
 Document doc = parser.getDocument()
 
-def result = xpathExpression.evaluate(doc, XPathConstants.NODE);
-
 NodeList rows = doc.getElementsByTagName("tr");
-Node nodes = (NodeList) result
 verifyRow(rows.item(0), "ClassName.CommandName", "Parameter 1", "Parameter 2");
 verifyRow(rows.item(1), "AnyCommand.anyCommand","firstParameterDescription","secondParameterDescription")
 
